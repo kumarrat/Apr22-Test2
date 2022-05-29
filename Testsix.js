937e834b01c56d3b0b847ddf98a8ef690d840c24
Kumar Ratnesh <102251616+kumarrat@users.noreply.github.com>
2022-05-29 15:21:32 +0530

    Add files via upload

---
+++ Testsix.js
@@ -0,0 +1,49 @@
+console.log('Test Six')
+
+let day = 7;
+
+switch (day) {
+    case 1:
+        console.log('Sunday');
+        break;
+        
+        case 2:
+            console.log('Monday');
+            break;
+
+            case 3:
+                console.log('Tuesday');
+                break;
+
+                case 4:
+                    console.log('Wednesday');
+                    break;
+
+                    case 5:
+                        console.log('Thrusday');
+                        break;
+
+                        case 6:
+                            console.log('Friday');
+                            break;
+
+                            case 7:
+                                console.log('Saturday');
+                                break;
+
+                                default:
+                                    console.log('Invalid Input')
+                        
+}
+
+// check if a number is odd or even using if else
+
+let number = 1;
+
+if (number % 2 === 0) {
+    console.log('Number is even');
+
+} else {
+    console.log('Number is odd');
+}
+
